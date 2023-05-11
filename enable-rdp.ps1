07a410337abc960d826f74675e1bf888d9b09496
Lehny <95946648+Lehny@users.noreply.github.com>
2023-05-11 18:40:46 +0700

    Update enable-rdp.ps1

--- enable-rdp.ps1
+++ enable-rdp.ps1
@@ -6,5 +6,5 @@ write-host Password: Password@001
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
-.\ngrok.exe config add-authtoken 2KPMbPA65fazDtxaednaUWHxPqJ_2pyvc4PxG2fPkeAmLom6Y
+.\ngrok.exe config add-authtoken 2PdS486K8oVWHD5mBKpQCQXODsY_qgWo8CEn5WHSCgGL3bDT
 .\ngrok.exe tcp 3389
